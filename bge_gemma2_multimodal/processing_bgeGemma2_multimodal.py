25e2572c20eebfded73c175adaee855a35587e53
swan.blanc <swan.blanc@infomaniak.com>
2024-12-27 11:18:44 +0100

    finish the preprocess

--- bge_gemma2_multimodal/processing_bgeGemma2_multimodal.py
+++ bge_gemma2_multimodal/processing_bgeGemma2_multimodal.py
@@ -2,7 +2,7 @@
 Processor class for BgeGemma2Multimodal.
 """
 
-from typing import List, Union
+from typing import List, Union, Optional
 
 from transformers import GemmaTokenizerFast, SiglipImageProcessor
 from transformers.feature_extraction_utils import BatchFeature
@@ -12,6 +12,9 @@ from transformers.processing_utils import (
     ProcessorMixin,
     Unpack,
     _validate_images_text_input_order,
+    ProcessingKwargs,
+    TextKwargs,
+    ImagesKwargs,
     )
 from transformers.tokenization_utils_base import (
     PreTokenizedInput,
@@ -21,7 +24,26 @@ from transformers.utils import logging
 
 logger = logging.get_logger(__name__)
 
-IMAGE_TOKEN = "<vision>"
+
+class BgeGemma2MultimodalTextKwargs(TextKwargs):
+    instruct: Optional[Union[TextInput, PreTokenizedInput, List[TextInput], List[PreTokenizedInput]]]
+
+
+class BgeGemma2MultimodalImagesKwargs(ImagesKwargs):
+    do_convert_rgb: Optional[bool]
+
+
+class BgeGemma2MultimodalProcessorKwargs(ProcessingKwargs, total=False):
+    text_kwargs: BgeGemma2MultimodalTextKwargs
+    images_kwargs: BgeGemma2MultimodalImagesKwargs
+    _defaults = {
+        "text_kwargs":   {
+            "padding": False,
+            },
+        "images_kwargs": {
+            "data_format": "channels_first",
+            },
+        }
 
 
 # Copied from transformers.models.idefics2.processing_idefics2.is_url
@@ -86,6 +108,9 @@ def make_batched_images(images) -> List[List[ImageInput]]:
 
 # Adapt from transformers.models.paligemma.processing_paligemma.PaliGemmaProcessor
 class BgeGemma2MultimodalProcessor(ProcessorMixin):
+    IMAGE_TOKEN = "<vision>"
+    QUERY_TOKEN = "<query>"
+    TEXT_INSTRUCT_TOKEN = "<instruct>"
     attributes = ["image_processor", "tokenizer"]
     image_processor_class = "SiglipImageProcessor"
     tokenizer_class = ("GemmaTokenizer", "GemmaTokenizerFast")
@@ -101,11 +126,48 @@ class BgeGemma2MultimodalProcessor(ProcessorMixin):
         if tokenizer is None:
             tokenizer = GemmaTokenizerFast.from_pretrained("./bge_gemma2_multimodal_hub_files")
         self.image_seq_length = 1
-        assert IMAGE_TOKEN in tokenizer.all_special_tokens
-        self.image_token_id = tokenizer.convert_tokens_to_ids(IMAGE_TOKEN)
+        assert self.IMAGE_TOKEN in tokenizer.all_special_tokens
+        self.image_token_id = tokenizer.convert_tokens_to_ids(self.IMAGE_TOKEN)
         self.tokenizer = tokenizer
         self.image_processor = image_processor
         super().__init__(image_processor, tokenizer)
+
+    def format_text_input(self, text: TextInput = None, image=None, instruct: TextInput = None) -> TextInput:
+        # Image only embedding
+        if text is None:
+            # Query image embedding
+            if isinstance(instruct, str):
+                text = self.IMAGE_TOKEN + instruct if self.IMAGE_TOKEN not in instruct else instruct
+
+            # index image embedding
+            else:
+                text = self.IMAGE_TOKEN
+        # text only embedding
+        elif image is None:
+            # Query text embedding
+            if isinstance(instruct, str):
+                text = self.TEXT_INSTRUCT_TOKEN + instruct \
+                    if self.TEXT_INSTRUCT_TOKEN not in instruct else instruct
+        # Image and text embedding
+        else:
+            # Query text/image embedding
+            if isinstance(instruct, str):
+                # Add instruct token
+                # format: <instruct>Given a web search query, retrieve...
+                text = self.TEXT_INSTRUCT_TOKEN + instruct \
+                    if self.TEXT_INSTRUCT_TOKEN not in instruct else instruct
+
+                # Add image vision token
+                # format: <vision><instruct>Given a web search query, retrieve...
+                text = self.IMAGE_TOKEN + text if self.IMAGE_TOKEN not in text else text
+
+                # Add text query text
+                # format: <vision><instruct>Given a web search query, retrieve...<query>Find similar color image
+                text = self.QUERY_TOKEN + text if self.QUERY_TOKEN not in text else text
+            # text and image embedding
+            else:
+                text = self.IMAGE_TOKEN + text if self.IMAGE_TOKEN not in text else text
+        return text
 
     def __call__(
             self,
@@ -123,92 +185,48 @@ class BgeGemma2MultimodalProcessor(ProcessorMixin):
                 tokenizer_init_kwargs=self.tokenizer.init_kwargs,
                 **kwargs,
                 )
-        suffix = output_kwargs["text_kwargs"].pop("suffix", None)
+        instruct = output_kwargs["text_kwargs"].pop("instruct", None)
 
-        return_token_type_ids = True if suffix is not None else False
+        if not (images or text):
+            raise ValueError("You have to specify either `text` or `images`.")
 
-        if images is None:
-            raise ValueError("`images` are expected as arguments to a `PaliGemmaProcessor` instance.")
-        if text is None:
-            logger.warning_once(
-                    "You are using PaliGemma without a text prefix. It will perform as a picture-captioning model."
-                    )
-            text = ""
-
+        # Ensure `text` is a list of inputs
         if _is_str_or_image(text):
             text = [text]
-        elif isinstance(text, list) and _is_str_or_image(text[0]):
-            pass
 
-        if text is not None and images is not None:
-            if not any(IMAGE_TOKEN in sample for sample in text):
-                logger.warning(
-                        "You are passing both `text` and `images` to `PaliGemmaProcessor`. The processor expects special "
-                        "image tokens in the text, as many tokens as there are images per each text. It is recommended to "
-                        f"add `{IMAGE_TOKEN}` tokens in the very beginning of your text. For this call, we will infer how many images "
-                        "each text has and add special tokens."
-                        )
+        # Format each text
+        text = [self.format_text_input(t, images, instruct) for t in text]
 
-                if isinstance(text, List) and isinstance(images, List):
-                    if len(images) != len(text):
-                        raise ValueError(
-                                f"Received {len(images)} images for {len(text)} prompts. Each prompt should be associated with an image or list of images."
-                                )
-
-                # make a nested list of lists to be able to iterate over the images and text below
-                if is_valid_image(images):
-                    images = [[images]]
-                elif isinstance(images, list) and is_valid_image(images[0]):
-                    images = [[image] for image in images]
-                elif not (isinstance(images, list) and isinstance(images[0], list) and is_valid_image(images[0][0])):
-                    raise ValueError("images must be an image, list of images or list of list of images")
-
-                if suffix is not None and _is_str_or_image(suffix):
-                    suffix = [suffix]
-                if suffix is not None:
-                    suffix = [sfx + self.tokenizer.eos_token for sfx in suffix]
-
-                input_strings = [
-                    build_string_from_input(
-                            prompt=prompt,
-                            bos_token=self.tokenizer.bos_token,
-                            image_seq_len=self.image_seq_length,
-                            image_token=IMAGE_TOKEN,
-                            num_images=len(image_list) if isinstance(image_list, list) else 1,
-                            )
-                    for prompt, image_list in zip(text, images)
-                    ]
-                images = make_batched_images(images)
-            else:
-                expanded_samples = []
-                for sample in text:
-                    expanded_sample = sample.replace(IMAGE_TOKEN, IMAGE_TOKEN * self.image_seq_length)
-                    bos_rfind_index = expanded_sample.rfind(IMAGE_TOKEN)
-                    bos_index = bos_rfind_index + len(IMAGE_TOKEN) if bos_rfind_index != -1 else 0
-                    expanded_sample = (
-                            expanded_sample[:bos_index] + self.tokenizer.bos_token + expanded_sample[bos_index:]
+        # Check if text and images are both provided and have mismatched lengths
+        if text and images and isinstance(text, List) and isinstance(images, List) and len(images) != len(text):
+            raise ValueError(
+                    f"Received {len(images)} images for {len(text)} prompts. Each prompt should be associated with an image or list of images."
                     )
-                    expanded_samples.append(expanded_sample)
-                input_strings = [f"{sample}\n" for sample in expanded_samples]
-        pixel_values = self.image_processor(images, **output_kwargs["images_kwargs"])["pixel_values"]
+
+        # preprocess images
+        if images:
+            # make a nested list of lists to be able to iterate over the images and text below
+            if is_valid_image(images):
+                images = [[images]]
+            elif isinstance(images, list) and is_valid_image(images[0]):
+                images = [[image] for image in images]
+            elif not (isinstance(images, list) and isinstance(images[0], list) and is_valid_image(images[0][0])):
+                raise ValueError("images must be an image, list of images or list of list of images")
+            # TODO je pensse pas nessesaire
+            images = make_batched_images(images)
+            pixel_values = self.image_processor(images, **output_kwargs["images_kwargs"])["pixel_values"]
+        else:
+            pixel_values = None
 
         # max_length has to account for the image tokens
         if output_kwargs["text_kwargs"].get("max_length", None) is not None:
             output_kwargs["text_kwargs"]["max_length"] += self.image_seq_length
 
         inputs = self.tokenizer(
-                input_strings,
-                text_pair=suffix,
-                return_token_type_ids=return_token_type_ids,
+                text,
                 **output_kwargs["text_kwargs"],
                 )
-
-        return_data = {**inputs, "pixel_values": pixel_values}
-
-        if return_token_type_ids:
-            labels = inputs["input_ids"].masked_fill(inputs["token_type_ids"] == 0, -100)
-            return_data.update({"labels": labels})
-        return BatchFeature(data=return_data)
+        return BatchFeature(data={**inputs, "pixel_values": pixel_values})
 
     # Copied from transformers.models.clip.processing_clip.CLIPProcessor.batch_decode with CLIP->Gemma
     def batch_decode(self, *args, **kwargs):
